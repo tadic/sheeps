e9ce52937f25a718d26ca328f2ae92cb72dd38c0
tadic <ivantadic014@gmail.com>
2014-02-22 21:53:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sheeps::Application.config.secret_key_base = '9a93e7d1b85496fddb054bc066983b7a0f95ffde88cd7fb3af37079c4f528bb1b3e67526396ad80a21d5fcbeee21ba04f20c1ba69ef6fe9cb7fe14e04483ad5c'
