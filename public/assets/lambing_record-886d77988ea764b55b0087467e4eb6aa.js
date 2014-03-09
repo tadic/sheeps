09b66022037a3b70299c1bd97b7d705ca0064f46
tadic <ivantadic014@gmail.com>
2014-03-09 22:16:14 +0200

    sdasd

---
+++ public/assets/lambing_record-886d77988ea764b55b0087467e4eb6aa.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+
+ $(".select").select2();
+
+    $('[data-behaviour~=datepicker]').datepicker({
+       language: "rs",
+       format: 'dd-mm-yyyy',
+       autoclose: true
+    });
+
+    $('.multiselect').multiselect({
+        maxHeight: 400,
+         onChange: function(element, checked) {
+                add_remove_sheep_row(element.val());
+        },
+        includeSelectAllOption : true,
+        selectAllText : ' Selektuj sve',
+        selectAllValue : 'multiselect-all',
+        enableFiltering: true,
+        nonSelectedText: 'Nista selektovano'
+    });
+
+    $(document).on('change','.lambNum',function() {
+            set_lamb_rows_for_sheep($(this).val(),$(this).attr('id'));
+    });
+    $(document).on('keyup','#decimal',function(e) {
+        val= $(this).val();
+        a = val.match(/^[0-9]{1,4}\.[0-9]{1,3}$/) ===null;
+
+        if (a) {
+            $(this).addClass('invalid');
+        } else {
+            $(this).removeClass('invalid');
+        }
+    });
+});
+
+function add_remove_sheep_row(el){  
+    var values = $('.multiselect').val();
+    index = jQuery.inArray( el, values );
+    hiden_row =$('#sheep_row').first(); //the hiden one
+    if (index != -1) {
+        $new_row = hiden_row.clone();
+        $new_row.find('a').html('Ovca ' + el);
+        $new_row.find('.lambing_list').attr( 'id', 'lambing_list_'+el);    
+        $new_row.find('.lambNum').attr( 'id', el);
+        $new_row.show();
+        $new_row.attr('class', 'table table-hover');
+        $new_row.attr('id', 'sheep_row_'+el);
+        $new_row.appendTo("#accordion");          
+    } else {
+        $("#accordion").find("#sheep_row_"+el).remove();
+    }     
+}
+function set_lamb_rows_for_sheep(n, sheep_code){
+    list = '';
+
+    for (i=0; i<n; i++){
+
+        list += "<tr><td><input type='hidden' name='lambings[][sheep_code]' style='width: 50px' value='"+sheep_code+"'></td>"+
+                "<td><select class='btn' name='lambings[][sex]'><option>neutvrdjen</option><option>musko</option><option>zensko</option></select></td>"+
+                "<td><input id='decimal' name='lambings[][weight]' style='width: 50px' /></td>" +
+                "<td><input type='checkbox' value='true'  name='lambings[][alive]' checked></td>"+
+                "<td><input name='lambings[][describe]' style='width: 280px'></td>"+
+                "<td><input name='lambings[][comment]' style='width: 280px'></td></tr>";
+    }
+    table = $('#accordion').first();
+    table.find('#lambing_list_'+ sheep_code).html(list);
+}
+
+
+
+
+;
---
+++ public/assets/sheep_purchase-19f8791b09ec312b02c9228e6a7fb8fd.js
@@ -0,0 +1,43 @@
+
+$("#quantity").ready(function(){
+   $("#quantity").keyup(function (e) {
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        $("#errmsg").html("!").show().fadeOut("slow");
+        $(this).val('');
+               return false;
+    }
+    set_sheep_rows(parseInt($(this).val()));
+   });
+    
+    
+    
+});
+
+
+function set_sheep_rows(rows_number){
+    all_rows='';
+    row="<tr><td><input type='text' name='purchases[][sheep_code]' style='width: 70px'></td>"+
+            "<td><input type='text' name='purchases[][nickname]' style='width: 70px'></td>"+
+            "<td><select class='btn' name='purchases[][sex]'><option>musko</option><option>zensko</option></select></td>"+
+            "<td><input type='text' name='purchases[][percent_of_r]' style='width: 40px'>%</td>"+
+            "<td><input type='text' name='purchases[][price]' style='width: 40px'></td>"+
+            "<td><input type='text' name='purchases[][date_of_birth]'data-behaviour='datepicker' format='dd-mm-yyyy' style='width: 90px'></td>"+
+            "<td><input type='text' name='purchases[][father_code]' style='width: 70px'></td>"+
+            "<td><input type='text' name='purchases[][mother_code]' style='width: 70px'></td>"+
+            "<td><input type='text' name='purchases[][sheep_describe]' style='width: 280px'></td>"+
+            "<td><input type='text' name='purchases[][purchase_comment]' style='width: 120px'></td></tr>";
+            
+                
+    for (i=0; i<rows_number; i++){
+        all_rows += row;
+    }
+   $(".purchasing_list").html(all_rows);
+
+    $('[data-behaviour~=datepicker]').datepicker({
+       language: "rs",
+       format: 'dd-mm-yyyy',
+       autoclose: true
+    });
+    $('[data-behaviour~=datepicker]').datepicker('refresh');
+}
+;
