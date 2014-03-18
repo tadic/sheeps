b453395585a5191668bd53750ff5d0508450b196
tadic <ivantadic014@gmail.com>
2014-03-18 14:30:32 +0200

    delete all js

--- public/assets/uginuce-ae3419abdcf735718edbfddb656c54c9.js
+++
@@ -1,1 +0,0 @@
-function refresh_calendar(){$("[data-behaviour~=datepicker]").datepicker({language:"rs",format:"dd-mm-yyyy",autoclose:!0}),$("[data-behaviour~=datepicker]").datepicker("refresh")}$("#new_uginuce").ready(function(){refresh_calendar(),$(".select").select2()});
