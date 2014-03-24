c88f7d28fbc75a667ac20fdf5cd000233dc480f4
tadic <ivantadic014@gmail.com>
2014-03-24 21:51:21 +0200

    asda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Sheeps::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # Compress JavaScripts and CSS.
   # ???config.assets.js_compressor = :uglifier
@@ -61,7 +61,7 @@ Sheeps::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( *.js, /highcharts/*.js )
-config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
