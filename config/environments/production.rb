dcb9f63c2174e509f07d76260e702ed7d65aed94
tadic <ivantadic014@gmail.com>
2014-03-24 21:48:18 +0200

    asda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Sheeps::Application.configure do
 
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # ???config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -61,7 +61,7 @@ Sheeps::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( *.js, /highcharts/*.js )
-
+config.assets.js_compressor = Uglifier.new(mangle: false)
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
